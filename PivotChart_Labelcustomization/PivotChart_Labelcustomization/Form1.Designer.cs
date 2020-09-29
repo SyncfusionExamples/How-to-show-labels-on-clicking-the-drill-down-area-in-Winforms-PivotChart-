cc91ee97e8be497e2fe6f3d9939f558c11b23c55
Vijayalakshmi <vijayalakshmiroopkumar@syncfusion.com>
2020-09-29 17:37:36 +0530

    WF-56296 sample committed

---
+++ PivotChart_Labelcustomization/PivotChart_Labelcustomization/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace PivotChart_Labelcustomization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pivotChart1 = new Syncfusion.Windows.Forms.PivotChart.PivotChart();
+            this.SuspendLayout();
+            // 
+            // pivotChart1
+            // 
+            
+            this.pivotChart1.ChartTypes = Syncfusion.Windows.Forms.PivotChart.PivotChartTypes.Column;
+            this.pivotChart1.CustomPalette = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(165)))), ((int)(((byte)(165))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(114)))), ((int)(((byte)(196))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(173)))), ((int)(((byte)(71)))))};
+            this.pivotChart1.DeferLayoutUpdate = false;
+            this.pivotChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pivotChart1.Location = new System.Drawing.Point(0, 0);
+            this.pivotChart1.MinimumSize = new System.Drawing.Size(300, 250);
+            this.pivotChart1.Name = "pivotChart1";
+            this.pivotChart1.ShowPivotTableFieldList = false;
+            this.pivotChart1.Size = new System.Drawing.Size(1070, 530);
+            this.pivotChart1.TabIndex = 0;
+            this.pivotChart1.Text = "pivotChart1";
+            this.pivotChart1.UpdateManager = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CaptionBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(158)))), ((int)(((byte)(218)))));
+            this.CaptionButtonColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1070, 530);
+            this.Controls.Add(this.pivotChart1);
+            this.Name = "Form1";
+            this.Text = "PivotChartLabelCustomization";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.PivotChart.PivotChart pivotChart1;
+    }
+}
+
---
+++ PivotChart_Labelcustomization/PivotChart_Labelcustomization/Form1.cs
@@ -0,0 +1,78 @@
+﻿using Syncfusion.PivotAnalysis.Base;
+using Syncfusion.Windows.Forms;
+using Syncfusion.Windows.Forms.Chart;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PivotChart_Labelcustomization
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePivotChart();
+            this.StartPosition = FormStartPosition.CenterScreen;     
+        }
+
+        private void InitializePivotChart()
+        {
+            // Adding ItemSource to the Control
+            this.pivotChart1.ItemSource = ProductSalesData.GetSalesData();
+            this.pivotChart1.AllowDrillDown = true;
+
+            //Intialize the ChartRegionClick event
+          this.pivotChart1.ChartControl.ChartRegionClick += ChartControl_ChartRegionClick;
+
+            // Adding PivotAxis to the Control
+            this.pivotChart1.PivotAxis.Add(new PivotItem { FieldMappingName = "Product", TotalHeader = "Total" });
+            this.pivotChart1.PivotAxis.Add(new PivotItem { FieldMappingName = "Date", TotalHeader = "Total" });
+
+            // Adding PivotLegend to the Control
+            this.pivotChart1.PivotLegend.Add(new PivotItem { FieldMappingName = "Country", TotalHeader = "Total" });
+
+            // Adding PivotCalculations to the Control
+            this.pivotChart1.PivotCalculations.Add(new PivotComputationInfo { FieldName = "Quantity", Format = "#,##0" });
+           
+            this.pivotChart1.ShowLegend = true;
+
+            for (int i = 0; i < this.pivotChart1.ChartControl.Series.Count; i++)
+            {
+                // Enabling DisplayText
+                this.pivotChart1.ChartControl.Series[i].Style.DisplayText = true;
+
+                // Sets the Offset of the text from the position of the chart point.
+                this.pivotChart1.ChartControl.Series[i].Styles[0].TextOffset = 10.0F;
+
+                // Align the text of the series within the data point region.
+                this.pivotChart1.ChartControl.Series[i].Style.TextOrientation = ChartTextOrientation.Up;
+            }
+        }
+
+        //Invoke ChartRegionClick method to display text on clicking + and - button
+        private void ChartControl_ChartRegionClick(object sender, ChartRegionMouseEventArgs e)
+        {
+            if (e.Region != null && e.Region.Type == ChartRegionType.HorAxisLabel)
+            {
+                for (int i = 0; i < this.pivotChart1.ChartControl.Series.Count; i++)
+                {
+                    // Enabling DisplayText
+                    this.pivotChart1.ChartControl.Series[i].Style.DisplayText = true;
+
+                    // Sets the Offset of the text from the position of the chart point.
+                    this.pivotChart1.ChartControl.Series[i].Styles[0].TextOffset = 10.0F;
+
+                    // Align the text of the series within the data point region.
+                    this.pivotChart1.ChartControl.Series[i].Style.TextOrientation = ChartTextOrientation.Up;
+                }
+            }
+        }
+    }
+}
